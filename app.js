f9a52ba1244b398955c0366039f84d570534744a
Michael Gomez <michaelgomez@csus.edu>
2020-11-09 16:54:48 -0800

    Add landing page

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ var bodyParser = require("body-parser");
 var routes = require("./routes/routes.js");
 var app = express();
 
+app.use(express.static("static"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 routes(app);
 
 var server = app.listen(3000, function () {
-    console.log("app running on port.", server.address().port);
+  console.log("app running on port.", server.address().port);
 });
--- routes/routes.js
+++ routes/routes.js
@@ -1,81 +1,60 @@
 // inlcude data file
-const data = require('../data.json')
+const data = require("../data.json");
+var path = require("path");
 
-var appRouter = function (app) 
-{
-	app.get("/", function (req, res) 
-	{
-    		res.status(200).send({ message: 'Welcome to our restful API' });
-  	});
+var appRouter = function (app) {
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../static", "index.html"));
+  });
 
+  // adding get requests
 
-	// adding get requests
-	
-	app.get("/api/awards", function (req, res)
-	{
-		if(data == null)
-		{
-			res.send("File with awards is not found!");
-		}
-		else
-		{
-			res.send(data);
-		}
-	});
+  app.get("/api/awards", function (req, res) {
+    if (data == null) {
+      res.send("File with awards is not found!");
+    } else {
+      res.send(data);
+    }
+  });
 
-	app.get("/api/awards/:id", function (req, res)
-	{
-		const id = data[req.params.id-1];
-		if(id == null)
-		{
-			res.send("The award is not found!");
-		}
-		else
-		{
-			res.send(id);
-		}
-	});
+  app.get("/api/awards/:id", function (req, res) {
+    const id = data[req.params.id - 1];
+    if (id == null) {
+      res.send("The award is not found!");
+    } else {
+      res.send(id);
+    }
+  });
 
-	app.get("/api/award_categories", function (req, res)
-	{
-		award_cat = [];
-		let cat_data, cat_award;
-		let cond = false;
-		for(key in data)
-		{
-			cat_data = data[key].category;
-			for(val in award_cat)
-			{
-				cat_award = award_cat[val];
-				if(cat_award == cat_data)
-				{
-					cond = true;
-					break;
-				}
-				else
-				{
-					cond = false;
-				}
-			}
-			if(!cond)
-				award_cat.push(cat_data);
-		}
+  app.get("/api/award_categories", function (req, res) {
+    award_cat = [];
+    let cat_data, cat_award;
+    let cond = false;
+    for (key in data) {
+      cat_data = data[key].category;
+      for (val in award_cat) {
+        cat_award = award_cat[val];
+        if (cat_award == cat_data) {
+          cond = true;
+          break;
+        } else {
+          cond = false;
+        }
+      }
+      if (!cond) award_cat.push(cat_data);
+    }
 
-		res.send(award_cat);
-	});
-	
-	app.get("/api/award_categories/:id", function (req, res)
-	{
-		const id = award_cat[req.params.id-1];
-		if(id == null)
-		{
-			res.send("The award category is not found!");
-		}
-		else
-		{
-			res.send(id);
-		}
-	});
-}
+    res.send(award_cat);
+  });
+
+  app.get("/api/award_categories/:id", function (req, res) {
+    const id = award_cat[req.params.id - 1];
+    if (id == null) {
+      res.send("The award category is not found!");
+    } else {
+      res.send(id);
+    }
+  });
+};
 
 module.exports = appRouter;
