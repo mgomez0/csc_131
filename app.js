eabe1e1697c32dba60821b495f4f13c0ff092521
playlend <tarasyakovchuk@gmail.com>
2020-12-07 12:54:07 -0800

    the app is deployed (#24)
    * modified app.js to be compatible with web
    
    * added Procfile for heroku

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine','ejs');
 
 routes(app);
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   console.log("app running on port.", server.address().port);
 });
 
